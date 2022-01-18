597b2e09bb4ac2dff94508db6ac71c393e8ff03a
Anton Gordon <anton.gordon85@outlook.com>
2022-01-18 21:59:36 +0000

    raked models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bar38 = Restaurant.create([{name: "Bar38"}, {category: "italian"}, {address: "manchester"}])
